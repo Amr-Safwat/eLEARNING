06a61fefc747997ddc0cafbf93fa38887097fdab
Amr-Safwat <amrsafwat02@gmail.com>
2023-05-31 02:29:28 +0200

    About Us Section Created

--- js/main.js
+++ js/main.js
@@ -15,3 +15,13 @@ let ul = document.querySelector('nav ul');
 icon.onclick = () => {
     ul.classList.toggle('show');
 }
+
+let header = document.querySelector('header');
+
+window.onscroll = function () {
+    if(this.scrollY >= 200) {
+        header.classList.add('h-fixed');
+    } else {
+        header.classList.remove('h-fixed');
+    }
+}
